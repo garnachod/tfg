0f9de0c9d471646abeb8953bef6b26eafe37fe37
garnachod <garnachod@gmail.com>
2015-06-29 17:24:31 +0200

    inicio simplificación de la interfaz

---
+++ sources/Web/SupportWeb.py
@@ -0,0 +1,9 @@
+class SupportWeb(object):
+	"""Proporciona funciones utiles para mejorar la interfaz web"""
+	@staticmethod
+	def addGeneralStructure(cadena=""):
+		retorno = '''
+					<div class="mid">
+						<div class="mid-cont">
+				  '''
+		
