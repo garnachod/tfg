271068a1e3e24e3cde5b027314ba05df672baf56
Daniel Garnacho <garnachod@gmail.com>
2015-09-22 22:43:33 +0200

    Relevancia difusora terminada

--- sources/tests/relevanciaUsuarioPotencialDifusor.py
+++ sources/tests/relevanciaUsuarioPotencialDifusor.py
@@ -6,6 +6,22 @@ sys.path.append(lib_path)
 from DBbridge.ConsultasWeb import ConsultasWeb
 from Neo4j.ConexionNeo4j import ConexionNeo4j
 import datetime
+
+def getRTMByScreenName(consultas, screen_name):
+	#todos los tweets de un usuario
+	tweets = consultas.getTweetsUsuario(screen_name, limit=10000)
+	countRTs = 0.0
+
+	for tweet in tweets:
+		if tweet.orig_tweet != 0:
+			countRTs += 1
+
+	c = tweets[0].created_at - tweets[len(tweets)-1].created_at
+	if countRTs != 0.0:
+		return countRTs/c.days
+	else:
+		return 0.0
+
 if __name__ == '__main__':
 	user_id = 2383366169 #p_molins en otros test hacerlo con la cadena
 	grafo = ConexionNeo4j().getGraph()
@@ -23,16 +39,15 @@ if __name__ == '__main__':
 		if screen_name is None:
 			continue
 
-		#todos los tweets de un usuario
-		tweets = consultas.getTweetsUsuario(screen_name, limit=10000)
-		countRTs = 0.0
+		user = consultas.getUserByIDLargeCassandra(identificador)
+		
+		rtm = getRTMByScreenName(consultas, screen_name)
+		if rtm != 0.0:
+			#sumatorio de relevancia 
+			relevancia += (rtm) * user.followers
 
-		for tweet in tweets:
-			if tweet.orig_tweet != 0:
-				countRTs += 1
-
-		c = tweets[0].created_at - tweets[len(tweets)-1].created_at
-		if countRTs != 0.0:
-			relevancia += countRTs/c.days
-
-	print relevancia
+	screen_name = consultas.getScreenNameByUserID(user_id)
+	user = consultas.getUserByIDLargeCassandra(user_id)
+	rtm = getRTMByScreenName(consultas, screen_name)
+	#ultimos 2 terminos
+	print (relevancia + user.followers) * rtm
